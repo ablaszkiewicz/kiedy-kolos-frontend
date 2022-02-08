124b5100a119c2d5c1bce7a4f26db72b53ba4bec
bswitals <@=M=a6=>
2022-02-08 19:17:12 +0100

    Fix file tabbing

--- src/components/other/teacher/subjects/SubjectAdder.tsx
+++ src/components/other/teacher/subjects/SubjectAdder.tsx
@@ -3,40 +3,40 @@ import useSubjects from '../../../../hooks/useSubjects';
 import {useSetState} from "../../../../hooks/useSetState";
 
 interface State {
-	name: string;
-	shortName: string;
+  name: string;
+  shortName: string;
 }
 
 export const SubjectAdder = () => {
   const [state, setState] = useSetState({
-		name: '',
-		shortName: '',
-	} as State);
+    name: '',
+    shortName: '',
+  } as State);
   const { postMutation } = useSubjects();
 
   return (
-	<Flex borderWidth={'1px'} width={'30em'} m={2} p={2} borderRadius={5}>
-	  <Flex>
-		<Input
-			placeholder='Pełna nazwa...'
-			value={state.name}
-			onChange={(e) => setState({name: e.target.value})}
-			mr={2}
-		/>
-		<Input
-			placeholder='Krótka nazwa...'
-			value={state.shortName}
-			onChange={(e) => setState({shortName: e.target.value})}
-		/>
-	  </Flex>
+    <Flex borderWidth={'1px'} width={'30em'} m={2} p={2} borderRadius={5}>
+      <Flex>
+        <Input
+          placeholder='Pełna nazwa...'
+          value={state.name}
+          onChange={(e) => setState({name: e.target.value})}
+          mr={2}
+        />
+        <Input
+          placeholder='Krótka nazwa...'
+          value={state.shortName}
+          onChange={(e) => setState({shortName: e.target.value})}
+        />
+      </Flex>
 
-	  <Button
-		ml={2}
-		onClick={() => postMutation.mutate({ id: 0, name: state.name, shortName: state.shortName })}
-		isLoading={postMutation.isLoading}
-	  >
-		Dodaj
-	  </Button>
-	</Flex>
+      <Button
+        ml={2}
+        onClick={() => postMutation.mutate({ id: 0, name: state.name, shortName: state.shortName })}
+        isLoading={postMutation.isLoading}
+      >
+        Dodaj
+      </Button>
+    </Flex>
   );
 };
