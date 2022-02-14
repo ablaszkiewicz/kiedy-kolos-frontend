619bea6ea40ef061206444d87a80f7a331766e99
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-02-14 16:45:52 +0100

    Add default dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Register } from './pages/Register';
 import { TeacherPanel } from './pages/AdminPanel';
+import theme from './theme';
 
 const queryClient = new QueryClient();
 
