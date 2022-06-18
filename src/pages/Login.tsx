9672db7919b6955ebb4c1d0b4b072532b97e0752
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2022-06-18 23:21:26 +0200

    Improve browser detection

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,77 @@
-import { Box, Flex, Heading, Stack, useColorModeValue, Text } from '@chakra-ui/react';
-import { useGoogleOneTapLogin } from '@react-oauth/google';
+import { CopyIcon, LinkIcon } from '@chakra-ui/icons';
+import { Box, Flex, Heading, Stack, useColorModeValue, Text, Button, useToast } from '@chakra-ui/react';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import copy from 'copy-to-clipboard';
+import { detect } from 'detect-browser';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
 export const Login = () => {
+  const [supported, setSupported] = useState(false);
   const { googleLoginMutation } = useAuth();
   const { state: routerState } = useLocation();
   const bgColor = useColorModeValue('white', 'gray.700');
 
-  useGoogleOneTapLogin({
-    onSuccess: (credentialResponse) => {
-      googleLoginMutation.mutate(credentialResponse.credential!);
-      document.getElementById('g_a11y_announcement')!.style.height = '0px';
-    },
-    cancel_on_tap_outside: false,
-  });
+  const toast = useToast();
+
+  useEffect(() => {
+    const browser = detect();
+
+    if (
+      browser?.name.includes('webview') ||
+      browser?.name.includes('instagram') ||
+      browser?.name.includes('facebook')
+    ) {
+      setSupported(false);
+    } else {
+      setSupported(true);
+    }
+  }, []);
+
+  const copyToClipboard = () => {
+    copy('https://kiedy-kolos.pl/#/login');
+    toast({
+      title: 'Skopiowano link do schowka',
+      status: 'success',
+      duration: 2000,
+    });
+  };
+
+  const login = (response: CredentialResponse) => {
+    googleLoginMutation.mutate(response.credential!);
+    document.getElementById('g_a11y_announcement')!.style.height = '0px';
+  };
+
+  console.log(supported);
 
   return (
     <Flex minH={'100vh'} align={'center'} justify={'center'} bg={useColorModeValue('gray.50', 'gray.800')}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'}>Zaloguj się kontem google</Heading>
-          <Text fontSize={'lg'}>Powinieneś gdzieś zobaczyć baner do logowania</Text>
-        </Stack>
-        {routerState && (routerState as any).customMessage && (
-          <Box rounded={'lg'} bg={bgColor} boxShadow={'lg'} p={8}>
-            <Text>{(routerState as any).customMessage}</Text>
-          </Box>
+      <Stack align={'center'}>
+        {supported && (
+          <>
+            <Heading mb={4} fontSize={'4xl'}>
+              Zaloguj się kontem google
+            </Heading>
+            <GoogleLogin onSuccess={login} size={'medium'} auto_select={false} useOneTap />
+          </>
+        )}
+        {!supported && (
+          <>
+            <Heading> Przeglądarka nieobsługiwana</Heading>
+            <Text>Włącz stronę w pełnej wersji przeglądarki</Text>
+            <Button leftIcon={<LinkIcon />} onClick={copyToClipboard}>
+              Skopiuj link
+            </Button>
+          </>
         )}
       </Stack>
+
+      {routerState && (routerState as any).customMessage && (
+        <Box rounded={'lg'} bg={bgColor} boxShadow={'lg'} p={8}>
+          <Text>{(routerState as any).customMessage}</Text>
+        </Box>
+      )}
     </Flex>
   );
 };
